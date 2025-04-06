ff921ca7a284dc11954c8a976206202e61612476
HeyuanChi <heyuan.chi.de@gmail.com>
2025-04-06 20:24:03 +0200

    [ADD] demo

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 TEST_SCRIPT="./test.py"
 
 python $TEST_SCRIPT \
-    --dataroot /root/autodl-tmp/pytorch-CycleGAN-and-pix2pix/datasets/cat2dog \
-    --name latent-cat2dog \
-    --model latent_cycle_gan \
-    --num_test 1000
+    --dataroot ./datasets/example \
+    --name latent-example \
+    --model latent_example \
+    --num_test 10
--- train.sh
+++ train.sh
@@ -3,14 +3,14 @@
 TRAIN_SCRIPT="./train.py"
 
 python $TRAIN_SCRIPT \
-    --dataroot /root/autodl-tmp/pytorch-CycleGAN-and-pix2pix/datasets/cat2dog \
-    --name latent-cat2dog \
+    --dataroot ./datasets/example \
+    --name latent-example-new \
     --model latent_cycle_gan \
-    --lora_A_dir /root/autodl-tmp/diffusers/outputs/output-cat/checkpoint-5000 \
-    --lora_B_dir /root/autodl-tmp/diffusers/outputs/output-dog/checkpoint-5000 \
+    --lora_A_dir ./lora/exampleA \
+    --lora_B_dir ./lora/exampleB \
     --batch_size 16 \
     --init_with_cycle_gan True \
-    --cycle_gan_dir /root/autodl-tmp/pytorch-CycleGAN-and-pix2pix/checkpoints/cat2dog/ \
+    --cycle_gan_dir ./checkpoints/example/ \
     --n_epochs 40 \
     --n_epochs_decay 10 \
     --alpha_gan 1 \
