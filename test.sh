ad727717919a573ec1e78f598240f1b53de1f517
HeyuanChi <heyuan.chi.de@gmail.com>
2025-04-06 22:48:54 +0200

    [FEAT] demo

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ TEST_SCRIPT="./test.py"
 python $TEST_SCRIPT \
     --dataroot ./datasets/example \
     --name latent-example \
-    --model latent_example \
+    --model latent_cycle_gan \
     --num_test 10
